f6b93b24efac1f486eae4a231d282ace2e8f6e03
alikurniawan <alikoerniawan02@gmail.com>
2018-10-06 23:53:19 +0700

    nextPageAfterLoginPassed

--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
+//REQ
+// import { cards } from "./cards";
+
 // INPUTERS
 var iptUsername = document.querySelector("input#inputUsername");
 var loginPage = document.querySelector("#loginPage");
+var midArea = document.querySelector("#midArea");
 
 //BUTTONS
 var btnLogin = document.getElementById("btnLogin");
 var btnCancel = document.getElementById("btnCancel");
-var btnLogout = document.getElementById('btnLogout');
+var btnLogout = document.getElementById("btnLogout");
 
 //ACTIONS
 
-var afterLoginPass = function () {
-
-}
-
-
-
-btnLogin.addEventListener("click", function () {
+btnLogin.addEventListener("click", function() {
   const dbaseAPI = ["asdmin", "admin123"];
+  // var card = new cards();
 
   if (iptUsername.value.length <= 0) {
     alert("Jangan Kosong");
@@ -24,62 +23,20 @@ btnLogin.addEventListener("click", function () {
     sessionStorage.clear();
   } else if (iptUsername.value == dbaseAPI[0]) {
     sessionStorage.setItem("aloha", "sexy");
-    alert("god");
-
-    loginPage.innerHTML = `
-          <div class="container">
-          <div class="row">
-              <div class="col-12 mb-5">
-                  <!--dont distrub-->
-              </div>
-              <div id="loginPage" class="col">
-                  <div class="row justify-content-md-center">
-                      <div class="card card-header col-6 ">
-                          <div class="card card-header">
-                              <h4 class="d-flex justify-content-center">Successful Login</h4>
-                          </div>
-
-                          <div class="card card-body">
-                              <!-- --------INPUTERS------------------- -->
-                              <div class="ui huge icon input">
-                                  
-                              </div>
-                              <div class="ui huge icon input mt-3">
-                                  
-                              </div>
-                              <hr>
-                              <div class="">
-                                  <div class="ui buttons col align-self-end">
-                                  <button type="button" id="btnLogout" class="btn btn-success">Logout</button>
-                                  </div>
-                              </div>
-                              <!-- ------------INPUTERS--------------- -->
-                          </div>
-
-                      </div>
-                  </div>
-
-              </div>
-          </div>
-      </div>
-    `;
-
+    midArea.innerHTML = `<button id="btnLogout" type="button" class="btn btn-success">Logout</button>`;
   } else {
-    alert('Username and Password Salah')
+    alert("Username and Password Salah");
     sessionStorage.clear();
     window.location.reload(true);
   }
 });
 
-btnCancel.addEventListener("click", function () {
+btnCancel.addEventListener("click", function() {
   sessionStorage.clear();
   window.location.reload(true);
 });
 
-
-
-
-btnLogout.addEventListener("click", function () {
+btnLogout.addEventListener("click", function() {
   sessionStorage.clear();
   window.location.reload(true);
 });
