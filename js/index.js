5125d6c3a91ddd752a10e8a5a8934f80be536971
alikurniawan <alikoerniawan02@gmail.com>
2018-10-09 23:36:00 +0700

    Replace All

--- js/index.js
+++ js/index.js
@@ -3,66 +3,77 @@
 
 //Username Info
 
-
 // INPUTERS
 var iptUsername = document.querySelector("input#inputUsername");
 var loginPage = document.querySelector("#loginPage");
 var midAreaOne = document.querySelector("#midAreaOne");
-var inputOne = document.querySelector('#inputOne');
+var inputOne = document.querySelector("#inputOne");
 var inputTwo = document.querySelector("#inputTwo");
+var ip1 = document.querySelector("#ip1");
+var ip2 = document.querySelector("#ip2");
+var ip3 = document.querySelector("#ip3");
 
 //BUTTONS
 var btnLogin = document.getElementById("btnLogin");
 var btnCancel = document.getElementById("btnCancel");
 // var btnLogx = document.getElementById("btnLogx");
 
-
 //ACTIONS
 
-btnLogin.addEventListener("click", function () {
-  const dbaseAPI = ["asdmin", "admin123"];
-  // var card = new cards();
-
-  if (iptUsername.value.length <= 0) {
-    alert("Jangan Kosong");
-    window.location.reload(true);
-    sessionStorage.clear();
-  } else if (iptUsername.value == dbaseAPI[0]) {
-
-    inputOne.remove()
-
-    sessionStorage.setItem("aloha", "sexy");
-    // midArea.innerHTML = `
-    // <button id="btnLogout" type="button" 
-    // class="btn btn-success">Logout</button>
-    // `;
-    const btnLogOut = document.createElement('button');
-    const btnLabelOut = document.createTextNode('Logout');
-    btnLogOut.appendChild(btnLabelOut);
-    inputTwo.appendChild(btnLogOut);
-
-    btnLogOut.classList.add('btn', 'btn-success');
-    btnLogOut.setAttribute('id', 'btnLogx');
-
-    $(document).ready(function () {
-      $("#btnLogx").click(function () {
-        sessionStorage.clear();
-        $("#inputTwo").slideUp()
-        setTimeout(function () {
-          window.location.reload(true);
-        }, 2000);
-      })
-    })
-
-  } else {
-    alert("Username and Password Salah");
-    sessionStorage.clear();
-    window.location.reload(true);
-  }
+$(document).ready(function() {
+  $("#btnLogin").click(function() {
+    if (iptUsername.value.length <= 0) {
+      alert("kosong");
+    } else if (iptUsername.value == "de") {
+      $("#inputOne")
+        .slideUp(600)
+        .slideDown(600, function() {
+          inputOne.innerHTML = `<div class="alert alert-success" role="alert">
+          Selamat, Usrnama dan Password anda Benar !!
+        </div>`;
+        });
+      $("#inputTwo").show(600, function() {
+        inputTwo.innerHTML = `
+        <div class="alert alert-success" role="alert">
+        Page Next
+        </div>
+        `;
+      });
+    } else {
+      window.location.reload(true);
+      window.sessionStorage.clear();
+      alert("salah");
+    }
+  });
 });
 
+// sessionStorage.clear();
+// window.location.reload(true)
 
-btnCancel.addEventListener("click", function () {
-  sessionStorage.clear();
-  window.location.reload(true);
-});
+// btnCancel.addEventListener("click", function () {
+//   sessionStorage.clear();
+//   window.location.reload(true);
+// });
+
+// if (iptUsername.value.length <= 0) {
+//   alert("Jangan Kosong");
+//   window.location.reload(true);
+//   sessionStorage.clear();
+// }else if (iptUsername.value == dbaseAPI[0]){
+//   inputOne.slideUp();
+
+//   sessionStorage.setItem("aloha", "sexy");
+
+//   inputTwo.innerHTML = `
+//   <button id="btnLogx" type="button"
+//   class="btn btn-success">Logout</button>
+//   `;
+
+//   $("#btnLogx").click(function () {
+//     $("inputTwo").slideUp(), setTimeout(function(){
+//       window.location.reload(true)
+//     }, 2000)
+//   }
+// }
+
+const dbaseAPI = ["asdmin", "admin123"];
