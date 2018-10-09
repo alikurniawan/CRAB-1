9c14d9a89e36832a7c9f26dd6c966f645cfa99a9
alikurniawan <alikoerniawan02@gmail.com>
2018-10-10 00:26:27 +0700

    Machine

--- js/index.js
+++ js/index.js
@@ -1,79 +1,20 @@
-//REQ
-// import { cards } from "./cards";
+//INPUTERS
+var inputUsername = document.querySelector('#inputUsername');
+var inputPassword = document.querySelector('#inputPassword');
+var btnLogin = document.querySelector('#btnLogin');
+var btnGoogle = document.querySelector('#btnGoogle');
 
-//Username Info
+// JQUERY ACT
+$(document).ready(function(){
 
-// INPUTERS
-var iptUsername = document.querySelector("input#inputUsername");
-var loginPage = document.querySelector("#loginPage");
-var midAreaOne = document.querySelector("#midAreaOne");
-var inputOne = document.querySelector("#inputOne");
-var inputTwo = document.querySelector("#inputTwo");
-var ip1 = document.querySelector("#ip1");
-var ip2 = document.querySelector("#ip2");
-var ip3 = document.querySelector("#ip3");
+    btnLogin.addEventListener('click', function(){
+        alert('wow');
+    })
 
-//BUTTONS
-var btnLogin = document.getElementById("btnLogin");
-var btnCancel = document.getElementById("btnCancel");
-// var btnLogx = document.getElementById("btnLogx");
-
-//ACTIONS
-
-$(document).ready(function() {
-  $("#btnLogin").click(function() {
-    if (iptUsername.value.length <= 0) {
-      alert("kosong");
-    } else if (iptUsername.value == "de") {
-      $("#inputOne")
-        .slideUp(600)
-        .slideDown(600, function() {
-          inputOne.innerHTML = `<div class="alert alert-success" role="alert">
-          Selamat, Usrnama dan Password anda Benar !!
-        </div>`;
-        });
-      $("#inputTwo").show(600, function() {
-        inputTwo.innerHTML = `
-        <div class="alert alert-success" role="alert">
-        Page Next
-        </div>
-        `;
-      });
-    } else {
-      window.location.reload(true);
-      window.sessionStorage.clear();
-      alert("salah");
-    }
-  });
-});
-
-// sessionStorage.clear();
-// window.location.reload(true)
-
-// btnCancel.addEventListener("click", function () {
-//   sessionStorage.clear();
-//   window.location.reload(true);
-// });
-
-// if (iptUsername.value.length <= 0) {
-//   alert("Jangan Kosong");
-//   window.location.reload(true);
-//   sessionStorage.clear();
-// }else if (iptUsername.value == dbaseAPI[0]){
-//   inputOne.slideUp();
-
-//   sessionStorage.setItem("aloha", "sexy");
-
-//   inputTwo.innerHTML = `
-//   <button id="btnLogx" type="button"
-//   class="btn btn-success">Logout</button>
-//   `;
-
-//   $("#btnLogx").click(function () {
-//     $("inputTwo").slideUp(), setTimeout(function(){
-//       window.location.reload(true)
-//     }, 2000)
-//   }
-// }
-
-const dbaseAPI = ["asdmin", "admin123"];
+    $("#btnGoogle").click(function(){
+        $("#btnGoogle").slideUp();
+        
+        $("#btnGoogle").css("background-color", "green");
+        $("#btnGoogle").slideDown();
+    })
+})
