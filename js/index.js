0fb3bf10623d3da66174e19b59f0efa10812d79f
alikurniawan <alikoerniawan02@gmail.com>
2018-10-11 11:16:39 +0700

    Machine

--- js/index.js
+++ js/index.js
@@ -1,20 +1,89 @@
 //INPUTERS
-var inputUsername = document.querySelector('#inputUsername');
-var inputPassword = document.querySelector('#inputPassword');
-var btnLogin = document.querySelector('#btnLogin');
-var btnGoogle = document.querySelector('#btnGoogle');
+let inputUsername = document.querySelector('#inputUsername');
+let inputPassword = document.querySelector('#inputPassword');
+let btnLogin = document.querySelector('#btnLogin');
+let btnGoogle = document.querySelector('#btnGoogle');
+let bodyOne = document.getElementById('bodyOne');
+let tableBelow = document.getElementById('tableBelow');
 
 // JQUERY ACT
-$(document).ready(function(){
 
-    btnLogin.addEventListener('click', function(){
-        alert('wow');
+var Person = {
+    objUser: 'administrator',
+    objPass: 'admin12345'
+}
+
+var SlideAction = {
+    slide1: `
+        <div class="ui icon message">
+            <i class="notched circle loading icon"></i>
+            <div class="content">
+            <div class="header">
+                Just one second
+            </div>
+            <p>We're fetching that content for you.</p>
+            </div>
+        </div>`
+}
+
+
+$(document).ready(function () {
+
+    $("#btnLogin").click(function () {
+        $("#bodyOne").slideUp();
+
+        bodyOne.innerHTML = SlideAction.slide1;
+
+        $("#bodyOne").slideDown().slideUp(setTimeout = 5000);
+        // bodyOne.innerHTML = `REDY`;
+        // $("#bodyOne").slideDown();    
+
+
+        $("#tableBelow").hide()
+        show(setTimeout = 5000)
+        tableBelow.innerHTML = `
+        <div id="tableIsi" class="col col-12">
+            <table class="ui celled table">
+                <thead>
+                <tr><th>Name</th>
+                <th>Age</th>
+                <th>Job</th>
+                </tr></thead>
+                <tbody>
+                <tr>
+                    <td data-label="Name">James</td>
+                    <td data-label="Age">24</td>
+                    <td data-label="Job">Engineer</td>
+                </tr>
+                <tr>
+                    <td data-label="Name">Jill</td>
+                    <td data-label="Age">26</td>
+                    <td data-label="Job">Engineer</td>
+                </tr>
+                <tr>
+                    <td data-label="Name">Elyse</td>
+                    <td data-label="Age">24</td>
+                    <td data-label="Job">Designer</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        `;
+
+        $("#tableIsi").slideDown()
+
+
+
+
+
     })
 
-    $("#btnGoogle").click(function(){
+    $("#btnGoogle").click(function () {
         $("#btnGoogle").slideUp();
-        
+
         $("#btnGoogle").css("background-color", "green");
+        // $("#btnGoogle Google").remove();
+
         $("#btnGoogle").slideDown();
     })
 })
